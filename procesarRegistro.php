71f31bb5759464b8702bb67af84a1ae40cf5424b
DauoDauo <44442267+DauoDauo@users.noreply.github.com>
2019-06-02 01:13:05 -0500

    validacion y encriptacion de los datos del usuario
    -validado de contraseñas
    -verificado de repeticion de usuarios en base de datos
    -validacion de los espacion vacios
    -encriptado de contraseña

--- procesarRegistro.php
+++ procesarRegistro.php
@@ -1,5 +1,7 @@
 <?php
+//conexion con database
 $conexion = mysqli_connect("localhost","root","","personas");
+//guardando las variables
 $nombres = $_POST["nombres"];
 $apellidos = $_POST["apellidos"];
 $dni = $_POST["dni"];
@@ -7,9 +9,46 @@ $distrito = $_POST["distrito"];
 $correo = $_POST["correo"];
 $clave = $_POST["clave"];
 
+//validacion de llenado de datos
+$req =(strlen($nombres)*strlen($apellidos)*strlen($dni)*strlen($distrito)*strlen($correo)*strlen($clave)) or die ('<script>
+alert("complete todos los campos del formulario");
+window.history.go(-1);
+</script>');
 
+//validacion de contraseña
+if($clave!=$repeticion){
+    echo'<script>
+    alert("Las contraseñas no coinciden; intente de nuevo chavalito");
+    window.history.go(-1);
+    </script>
+    ';
+    
+}
+//encriptado de contraseña
+$clave = md5($clave);
+//verificar si el usuario ya existe en el base de datos;
+$verificar = mysqli_query($conexion ,"SELECT*FROM datos WHERE correo ='$correo' or dni ='$dni'");
+if(mysqli_num_rows($verificar) > 0){
+    echo '<script>
+    alert("El usuario ya esta registrado, regrese para Loguearse");
+    window.history.go(-1);
+    </script>
+    ';
+    
+}else{
+    echo '<script>
+    alert("se ha registrado satisfactoriamente");
+    </script>
+    ';
+header("Location:login.php");
+}
+
+//insercion
 $insertar = "INSERT INTO datos(nombres,apellidos,dni,distrito,correo,claveUsuario) VALUE ('$nombres','$apellidos','$dni','$distrito','$correo','$clave')";
+
+//ejecucion
 $resultado = mysqli_query($conexion, $insertar);
+//reingreso al portal
 
 header("Location:portal.php");
 
--- registro.php
+++ registro.php
@@ -5,46 +5,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="styleregistro.css">
 </head>
 <body>
+    <h1>Bienvenido al centro de registros</h1>
 <form action="procesarRegistro.php" method="post">
     <div>
         <input type="hidden" name="id" >
     </div>
     <div>
-        <label for="">Nombres:</label>
-        <input type="text" name="nombres">
+        <label for="">NOMBRES:</label>
+        <input class="myinput" type="text" name="nombres" style="width:300px; height: 30px ; border: 1px solid blue "  >
     </div><br>
 
     <div>
         <label for="">APELLIDOS:</label>
-        <input type="text" name="apellidos">
+        <input type="text" name="apellidos" style="width:300px; height: 30px ; border: 1px solid blue" >
     </div><br>
 
     <div>
         <label for="">DNI:</label>
-        <input type="text" name="dni">
+        <input type="text" name="dni" style="width:300px; height: 30px ; border: 1px solid blue" >
     </div><br>
     <div>
         <label for="">DISTRITO:</label>
-        <input type="text" name="distrito">
+        <input type="text" name="distrito" style="width:300px; height: 30px ; border: 1px solid blue" >
     </div><br>
 
     <div>
-        <label for="">correo:</label>
-        <input type="email" name="correo">
+        <label for="">CORREO:</label>
+        <input type="email" name="correo" style="width:300px; height: 30px ; border: 1px solid blue" >
     </div><br>
    
     <div>
         <label for="">CONTRASEÑA:</label>
-        <input type="password" name="clave">
+        <input type="password" name="clave" style="width:300px; height: 30px ; border: 1px solid blue" >
     </div><br>
+
+    <div>
+    <label for="">CONFIRMAR CONTRASEÑA:</label>
+        <input type="password" name="repeticion" style="width:300px; height: 30px ; border: 1px solid blue" >
+    </div><br>
+
+    <div class="bot"><button class="boton">REGISTRARSE</button></div><br>
+
+    <div><h3>¿Ya estas registrado?</h3></div>
+
+    <div class="entrada"><a href="login.php">Haz clic aqui</a></div>
     
-    <div><button>REGISTRAR</button></div>
-    <p> ¿YA TIENE CONTRASEÑA?
-    </p>
-    <a href="login.php">Haz clic</a>
 
     
     
