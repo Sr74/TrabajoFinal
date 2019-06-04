f84ba01b02c9b100b9a9b281bb820ac027ec6206
Shamil Rivera Acacio <reyiv19@gmail.com>
2019-06-04 11:28:55 -0500

    page principal(mejora1)

--- portal.php
+++ portal.php
@@ -1,65 +1,120 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>RescuePets</title>
-    <link rel="stylesheet" href="portalstyle.css">
-    
-</head>
-<body>
-    <form action=""></form>
+    <title>RESCUEPETS</title>
 
-    <header>
-        <div>
-            <img src="https://www.logoground.com/uploads/2018124112018-03-164243733Pets.jpg" alt="logo" width="150" height="100">
+    <link rel="stylesheet" href="portalstyle.css">
+
+</head>
+
+<body>
+
+
+    <header class="main-header">
+
+        <div class="container">
+            <div class="logo-container column column--50">
+                <img src="https://i.pinimg.com/originals/6b/84/82/6b8482edfb38498c6b5aa6871567aa87.png" alt="logo" class="logo">
+            </div>
         </div>
 
-        <nav>
+        <nav class="main-nav">
+            <div class="main container">
+                <ul class="menu" id="menu">
+                    <li class="menu__item"><a href="" class="menu__link menu__link--select">Inicio</a></li>
+                    <li class="menu__item"><a href="aboutus.php" class="menu__link">Nosotros</a></li>
+                    <li class="menu__item"><a href="registro.php" class="menu__link">Registrate</a></li>
+                    <li class="menu__item"><a href="login.php" class="menu__link">Inicia Sesion</a></li>
+                    <li class="menu__item"><a href="" class="menu__link">Contactanos</a></li>
+                </ul>
+            </div>
 
-            <ul>
-                <li><a href="login.php">Inicia Sesión</a></li>
-                <li><a href="registro.php">Registrate</a></li>
-                <li><a href="aboutus.php">Sobre Nosotros</a></li>
-            </ul>
-          
         </nav>
-
     </header>
 
-    <div class="imagen">
-        <img src="https://www.losreplicantes.com/images/articulos/10000/10715/t5.jpg" alt="Mascotas">
-        <h2>Mas información</h2>
-    </div>
+
+    <section class="banner">
+        <img src="https://cdn.pixabay.com/photo/2018/08/12/16/59/ara-3601194_1280.jpg" alt="" class="banner__img">
+        <div class="banner__content"> RESCUEPETS: Salvemos a los animales </div>
+    </section>
+
+    <main>
+
+        <section class="group animals-rescue">
+            <h2 id="grupotitle">Tipos de Animales que rescatamos</h2>
+            <div class="grupotipo">
+                <div class="imge">
+                    <img src="https://images.pexels.com/photos/374825/pexels-photo-374825.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="perros" class="animals-rescue__img">
+                    <div class="descrip">
+                        <h6>Perros</h6>
+                        <p>Caninos de toda raza</p>
+                    </div>
+                </div>
+                <div class="imge">
+                    <img src="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="gatos" class="animals-rescue__img">
+                    
+                    <div class="descrip">
+                        <h6>Gatos</h6>
+                        <p>Caninos de toda raza</p>
+                    </div>
+                </div>
+                <div class="imge">
+                    <img src="https://images.pexels.com/photos/1059823/pexels-photo-1059823.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="loros" class="animals-rescue__img">
+                    <div class="descrip">
+                        <h6>Loros</h6>
+                        <p>Caninos de toda raza</p></div>
+                    </div>
+                <div class="imge">
+                    <img src="https://images.pexels.com/photos/104373/pexels-photo-104373.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=650&w=940" alt="conejos" class="animals-rescue__img">
+                    <div class="descrip">
+                        <h6>Conejos</h6>
+                        <p>Caninos de toda raza</p>
+                    </div>
+                </div>
+            </div>
+        </section>
 
 
-    
-</body>
-<footer>
+    </main>
 
-        <div>
-            <h5>Direccion:</h5>
-            <ul>
-                <li>ASan MART #544</li>
-                <li>eRF lOS ANGELS #764</li>
-            </ul>
-        </div>
-        <div>
-            <h5>Telefonos:</h5>
-            <ul>
-                <li>7675675675</li>
-                <li>54353453454</li>
-                <li>5345345368</li>
-            </ul>
-        </div>
-        <div>
-            <h5>Correo de Contacto</h5>
-            <ul>
-                <li>rey@gmail.com</li>
-                <li>dauosuarez@gmail.com</li>
-            </ul>
+
+
+
+    <footer class="main-footer">
+        <div class="container container--flex">
+            <div class="column column--33">
+                <h2 class="column__title">¿Donde puede encontrarnos?</h2>
+                <p class="column_txt">
+                    Ubiquenos en:
+                    <ul>
+                        <li><span class="icon-map">Av. La Molina 545 cerca a la USIL</span></li>
+                        <li><span class="icon-map"> Av. Alfredo Benavides 657 </span></li>
+                        <li>Jr. Los Pinos 675</li>
+                    </ul>
+                </p>
+            </div>
+            <div class="column column--33">
+
+                <h2 class="column__title">Organizaciones de ayuda Animal en Perú</h2>
+                <p class="column_txt"><a href="http://vozanimalperu.com/">Voz Animal</a></p>
+                <p class="column_txt"><a href="https://asppa-peru.webnode.es/">ASSPA PERU</a></p>
+                <p class="column_txt"><a href="http://patitasdeamor.com.pe/">Patitas de Amor</a></p>
+
+            </div>
+            <div class="column column--33">
+                <h2 class="column__title">Siguenos en nuestras Redes</h2>
+                <p class="column_txt"><a href=""><span class="icon-facebook"></span></a></p>
+                <p class="column_txt"><a href=""><span></span class="icon-instagram"></a></p>
+                <p class="column_text"><a href=""><span></span class="icon-whatsapp"></a></p>
+            </div>
+            <p class="copy"> © 2018 RESCUEPETS | Todos los derechos reservados</p>
         </div>
 
     </footer>
+</body>
+
 </html>
