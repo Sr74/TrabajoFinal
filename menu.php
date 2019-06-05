5bfde1be1512312d920bca06983576250e52ea9c
Shamil Rivera Acacio <reyiv19@gmail.com>
2019-06-04 23:28:14 -0500

    add pointer

--- menu.php
+++ menu.php
@@ -5,31 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="portalstyle.css">
+    <link rel="stylesheet" href="page2.css">
 </head>
 <STYLE>A {text-decoration: none;} </STYLE>
 <body>
-<header class="main-header">
 
-<div class="container">
-    <div class="logo-container column column--50">
-        <img src="https://i.pinimg.com/originals/6b/84/82/6b8482edfb38498c6b5aa6871567aa87.png" alt="logo" class="logo">
-    </div>
-</div>
-
-<nav class="main-nav">
-    <div class="main container">
-        <ul class="menu" id="menu">
-            <li class="menu__item"><a href="portal.php" class="menu__link menu__link--select"><h3>Inicio</h3></a></li>
-            <li class="menu__item"><a href="nosotros.php" class="menu__link"><h3>Nosotros</h3></a></li>
-            <li class="menu__item"><a href="registro.php" class="menu__link"><h3>Registrate</h3></a></li>
-            <li class="menu__item"><a href="login.php" class="menu__link"><h3>Inicia Sesion</h3></a></li>
-            <li class="menu__item"><a href="soporte.php" class="menu__link"><h3>Contactanos</h3></a></li>
+<div class="navigation">
+        <input type="checkbox">
+        <span></span>
+        <span></span>
+        <ul class="menu">
+            <li><a href="">Nosotros</a></li>
+            <li><a href="login.php">Inicia Sesion</a></li>
+            <li><a href="registro.php">Registrate</a></li>
         </ul>
     </div>
 
-</nav>
-</header>
     
 </body>
 </html>
