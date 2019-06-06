2a2eab3bb04f870338fc071b8507399873b0177c
alumno informatica <alumno@ciis.usil.edu.pe>
2019-06-06 15:29:32 -0500

    actualiza registro mascotas

---
+++ After/procesoregistrarmascota.php
@@ -0,0 +1,16 @@
+<?php
+$nombre = $_POST["nombre"];
+$numero = $_POST["numero"];
+$numero2 = $_POST["numero2"];
+$direc = $_POST["direc"];
+$descrip = $_POST["descrip"];
+$conexion = mysqli_connect("localhost","root","","personas");
+
+
+if($numero == $numero2){
+    $verificar = mysqli_query($conexion ,"INSERT INTO nombreexxxxxxx VALUES (NULL,'$nombre','$numero','$direc','$descrip')");
+    header("Location:adoptar.php");
+}else{
+    header("Location:registrarmascota.php");
+}
+?>
---
+++ After/registrarmascota.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="procesoregistrarmascota.php" method="post">
+        <div>
+            <label>Ingrese su nombre:</label>
+            <input type="text" name="nombre">
+        </div>
+        <div>
+            <label>Ingrese numero de telefono:</label>
+            <input type="text" name="numero">
+        </div>
+        <div>
+            <label>Ingrese numero de telefono nuevamente:</label>
+            <input type="text" name="numero2">
+        </div>
+        <div>
+            <label>Ingrese direccion:</label>
+            <input type="text" name="direc">
+        </div>
+        <div>
+            <label>Ingrese descripcion del animal:</label>
+            <input type="text-area" name="descrip">
+        </div>
+        <div>
+            <button>registrar</button>
+        </div>
+    
+    
+    </form>
+    
+</body>
+</html>
