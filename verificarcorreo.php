489791fff1e4f4101765049e76cf334af0b10b85
Jairo Castro <Jairo182278@gmail.com>
2019-06-05 22:56:20 -0500

    actulizar contraseña

--- verificarcorreo.php
+++ verificarcorreo.php
@@ -2,15 +2,15 @@
 $correo = $_POST["correo"];
 $conexion = mysqli_connect("localhost","root","","personas");
 $verificar = mysqli_query($conexion ,"SELECT*FROM datos WHERE correo ='$correo'"); 
-$resultado = $conexion->query("SELECT*FROM datos WHERE correo ='$correo'");
+
 if(mysqli_num_rows($verificar) > 0){
     $contra1 = $_POST["contra1"];
     $contra = $_POST["contra"];
     if($contra == $contra1){
-            header("Location:sds.php");
+        $verificar = mysqli_query($conexion ,"UPDATE datos SET claveUsuario='$contra' WHERE correo ='$correo'");
     }else{
             header("Location:contraseñamala.php");
-}
+}   
 }else{
     header("Location:contraseñamala.php");
 }
