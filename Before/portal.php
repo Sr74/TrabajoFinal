a4fff24c2719807830001ca9f359c76b157aec99
alumno informatica <alumno@ciis.usil.edu.pe>
2019-06-06 15:29:08 -0500

    Modificaciones en Before

--- Before/portal.php
+++ Before/portal.php
@@ -11,35 +11,41 @@
 <body>
     <header class="main-header">
 
-            <div class="logo-container column column--50">
-                <img src="https://i.pinimg.com/originals/6b/84/82/6b8482edfb38498c6b5aa6871567aa87.png" alt="logo" class="logo">
+            <div class>
+                <img class="logo" src="https://i.pinimg.com/originals/6b/84/82/6b8482edfb38498c6b5aa6871567aa87.png">
             </div>
 
-        <nav class="main-nav">
-                <ul class="menu" id="menu">
-                    <li class="menu__item"><a href="" class="menu__link menu__link--select">Inicio</a></li>
-                    <li class="menu__item"><a href="" class="menu__link">Nosotros</a></li>
-                    <li class="menu__item"><a href="" class="menu__link">Registrate</a></li>
-                    <li class="menu__item"><a href="" class="menu__link">Inicia Sesion</a></li>
-                    <li class="menu__item"><a href="" class="menu__link">Contactanos</a></li>
-                </ul>
+            <nav class="main-nav">
+                    <ul class="menu" id="menu">
+                        <li class="menu__item"><a href="" class="menu__link menu__link--select">Inicio</a></li>
+                        <li class="menu__item"><a href="" class="menu__link">Nosotros</a></li>
+                        <li class="menu__item"><a href="" class="menu__link">Registrate</a></li>
+                        <li class="menu__item"><a href="" class="menu__link">Inicia Sesion</a></li>
+                        <li class="menu__item"><a href="" class="menu__link">Contactanos</a></li>
+                    </ul>
+            </nav>
 
-        </nav>
-
-        <div class="menu-tog"><i class="fas fa-bars"></i></div>
+            //<div class="menu-tog"><i class="fas fa-bars"></i></div>
 
         </header>
 
 
         <section class="banner">
-        <img src="https://cdn.pixabay.com/photo/2018/08/12/16/59/ara-3601194_1280.jpg" alt="" class="banner__img">
+        <img src="https://images.pexels.com/photos/825947/pexels-photo-825947.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" class="banner-img">
         <div class="banner__content"> RESCUEPETS: Salvemos a los animales </div>
         </section>
 
-        <footer>
-            <div id="pie">
-                <p></p>
+
+        <section class="izq-descp">
+            <div>
+                <h1>RESCUEPETS: Más que una Familia</h1>
             </div>
-        </footer>
+
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi consectetur illo consequatur vel praesentium iste dolorum dignissimos officia, ratione provident odio molestiae accusamus enim harum quod corporis? Architecto, sunt quia.</p>
+            </div>
+            
+        </section>
+
 </body>
 </html>
