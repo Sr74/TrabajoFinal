7c072057f356a9efb613ef7d3ff15e25db860e5f
Jairo Castro <Jairo182278@gmail.com>
2019-06-06 23:56:55 -0500

    casinada

--- After/adoptar.php
+++ After/adoptar.php
@@ -9,9 +9,30 @@ $sql = "SELECT*FROM adoptados";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="page2.css">
 </head>
 <body>
-    <table>
+    <div class="navigation">
+        <input type="checkbox">
+        <span></span>
+        <span></span>
+        <ul class="menu">
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="registrarmascota.php">Registrar Mascota</a></li>
+            <li><a href="sobrePets.php">Sobre Pets</a></li>
+            <li><a href="donar.php">Donar</a></li>
+        </ul>
+    </div>
+
+    <table class="table">
+        <tr>
+            <td>Nombre</td>
+            <td>Numero</td>
+            <td>Direccion</td>
+            <td>Descripcion</td>
+        </tr>
+
+    
         <?php
         foreach($pdo->query($sql) as $fila){
         ?> 
--- After/registrarmascota.php
+++ After/registrarmascota.php
@@ -5,8 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="page2.css">
 </head>
 <body>
+    <div class="navigation">
+        <input type="checkbox">
+        <span></span>
+        <span></span>
+        <ul class="menu">
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="adoptar.php">Adoptar</a></li>
+            <li><a href="sobrePets.php">Sobre Pets</a></li>
+            <li><a href="donar.php">Donar</a></li>
+        </ul>
+    </div>
+
     <form action="procesoregistrarmascota.php" method="post">
         <div>
             <label>Ingrese su nombre:</label>
