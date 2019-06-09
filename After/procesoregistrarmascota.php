1d67acb23556ed38c61086bf14f7327d6577dbe5
Jairo Castro <Jairo182278@gmail.com>
2019-06-09 11:45:31 -0500

    condicion registro masc

--- After/procesoregistrarmascota.php
+++ After/procesoregistrarmascota.php
@@ -5,12 +5,17 @@ $numero2 = $_POST["numero2"];
 $direc = $_POST["direc"];
 $descrip = $_POST["descrip"];
 $conexion = mysqli_connect("localhost","root","","personas");
-
-
-if($numero == $numero2){
-    $verificar = mysqli_query($conexion ,"INSERT INTO adoptados VALUES (NULL,'$nombre','$numero','$direc','$descrip')");
-    header("Location:adoptar.php");
-}else{
+if($nombre == "" or $numero == "" or $direc == "" or $descrip == ""){
     header("Location:registrarmascota.php");
+}else{
+    if($numero == $numero2){
+        $verificar = mysqli_query($conexion ,"INSERT INTO adoptados VALUES (NULL,'$nombre','$numero','$direc','$descrip')");
+        header("Location:adoptar.php");
+    }else{
+        header("Location:registrarmascota.php");
+    }
 }
+
+
+
 ?>
