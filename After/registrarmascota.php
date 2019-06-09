f8acb8f5e0e7360505eee61d23f20b730810ab5e
DauoDauo <44442267+DauoDauo@users.noreply.github.com>
2019-06-08 21:48:27 -0500

    modificaciones de diseño

--- After/registrarmascota.php
+++ After/registrarmascota.php
@@ -6,6 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="page2.css">
+    <style>
+        body{
+            background-image: url(https://encolombia.com/wp-content/uploads/2017/08/tienda-mascotas-medellin.jpg);
+        }
+        .mascota{
+            margin: 80px 300px;
+            border: 2px solid black;
+            padding: 20px 80px;
+            background-color: white;
+            border-radius: 10px;
+            
+        }
+        .boton{
+            width: 80px;
+            height: 30px;
+            color: orange;
+        }
+    </style>
 </head>
 <body>
     <div class="navigation">
@@ -22,34 +40,38 @@
             <li><a href="alertas.php">Alertas</a></li>
         </ul>
     </div>
-
+    <div class="mascota">
     <form action="procesoregistrarmascota.php" method="post">
+        <h1> REGISTRO DE MASCOTAS</h1>
         <div>
             <label>Ingrese su nombre:</label>
-            <input type="text" name="nombre">
-        </div>
+            <input type="text" name="nombre" style="width:300px; height: 30px ; border: 1px solid blue; border-radius: 10px">
+        </div><br>
         <div>
             <label>Ingrese numero de telefono:</label>
-            <input type="text" name="numero">
-        </div>
+            <input type="text" name="numero" style="width:300px; height: 30px ; border: 1px solid blue; border-radius: 10px">
+        </div><br>
         <div>
-            <label>Ingrese numero de telefono nuevamente:</label>
-            <input type="text" name="numero2">
-        </div>
+            <label>Reingrese su numero de telefono:</label>
+            <input type="text" name="numero2" style="width:300px; height: 30px ; border: 1px solid blue; border-radius: 10px">
+        </div><br>
         <div>
             <label>Ingrese direccion:</label>
-            <input type="text" name="direc">
-        </div>
+            <input type="text" name="direc" style="width:300px; height: 30px ; border: 1px solid blue; border-radius: 10px">
+        </div><br>
         <div>
-            <label>Ingrese descripcion del animal:</label>
+            <label>Ingrese descripcion del animal:</label><br>
             <textarea name="descrip" cols="30" rows="10"></textarea>
-        </div>
+        </div><br>
         <div>
-            <button>registrar</button>
+            <button class="boton">registrar</button>
         </div>
     
     
     </form>
+
+    </div>
+    
     
 </body>
 </html>
