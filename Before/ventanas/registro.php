084ccb0c39f5f5fbd71a2c15c3080ac9513120fa
DauoDauo <44442267+DauoDauo@users.noreply.github.com>
2019-06-10 03:01:21 -0500

    validando

--- Before/ventanas/registro.php
+++ Before/ventanas/registro.php
@@ -11,38 +11,40 @@
 <?php include('../partes/layerizq.php'); ?>
 <?php include('../partes/headpart.php'); ?>
 <div class="formulario">
-
-    <form action="procesarRegistro.php" method="post" class="form_register">
+    <div class="form_register">
         <h2 class="titulo">CREAR UNA CUENTA</h2>
         <div class="contenedor">
-         
+            <form action="procesarRegistro.php" method="post">
                 <input type="hidden" name="id" >
+                
+                <input type="text" name="nombres" class="input_100" placeholder="Nombres" >
             
-                <input type="text" name="nombres" class="input_100" placeholder="Nombres" required>
+                <input type="text" name="apellidos" class="input_100" placeholder="Apellidos"  >
+        
+                <input type="text" name="dni" class="input_48" placeholder="DNI" >
+        
+                <input type="text" name="distrito" class="input_48" placeholder="Distrito" >
+        
+                <input type="email" name="correo" class="input_100" placeholder="Correo Electrónico" >
+        
+                <input type="password" name="clave" class="input_48" placeholder="Contraseña"  >
             
-                <input type="text" name="apellidos" class="input_100" placeholder="Apellidos" required >
-          
-                <input type="text" name="dni" class="input_48" placeholder="DNI" required>
-          
-                <input type="text" name="distrito" class="input_48" placeholder="Distrito" required >
-           
-                <input type="email" name="correo" class="input_100" placeholder="Correo Electrónico" required>
-           
-                <input type="password" name="clave" class="input_48" placeholder="Contraseña"  required>
-            
-                <input type="password" name="repeticion" class="input_48" placeholder="Repita su contraseña" required>
-            
-        </div>
-        <div class="text">
-            <div ><button class="bott">REGISTRARSE</button></div><br>
-            <div><h3>¿Ya estas registrado?</h3></div>
-            <div class="entrada"><a href="login.php">Haz clic aqui</a></div><br>
+                <input type="password" name="repeticion" class="input_48" placeholder="Repita su contraseña" >
+                
+                <div class="bot"><button class="bott">REGISTRARSE</button></div>
+            </form>
+                <div class="text">
+                        <div><h3>¿Ya estas registrado?</h3></div>
+                        <div class="entrada"><a href="login.php">Haz clic aqui</a></div><br>
 
+                </div>
+            
         </div>
         
+        
+    </div>
 
-
-    </form>
+   
 
 </div>
 
