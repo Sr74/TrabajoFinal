597c4472463ade1be9f07d7f2e302caf18ce5439
Shamil Rivera Acacio <reyiv19@gmail.com>
2019-06-09 23:17:12 -0500

    Sobre pets medio

--- After/sobrePets.php
+++ After/sobrePets.php
@@ -4,25 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="SobrePets.css">
     <title>SobrePets</title>
-    <link rel="stylesheet" href="sobrepets.css">
+    
 </head>
 <body>
     <?php include ("menu.php") ?>
     <div class="main">
-        <span>
-            Recursos para las Mascotas
-        </span>
 
-        <div>
-            <p>En esta sección podra encontrar información importante <br> para que su mascota viva feliz, desde comida que le puede dar hasta preguntas frecuentes que nos realizan </p>
+        <div class="col1">
+            <span id="title">
+                Recursos para las Mascotas
+            </span>
+
+            <div class="descrip">
+                <p>En esta sección podra encontrar información importante <br> para que su mascota viva feliz, desde comida que le puede dar hasta preguntas frecuentes que nos realizan </p>
+            </div>
+
         </div>
+
+        <div class="col2">
+            <h3 id="name">Rocky</h3>
+            <p id="desc-name">Cuidame querido humano o sino llamo a Jhon Wick</p>
+        </div>
+        
     </div>
 
-    <div class="comida">
-        <span>
-            ¿Como alimentar a mi mascota?
-        </span>
+    <div class="main-comida">
+
+        <div class="inside">
+
+            <h2 id="title-comida"> Comida Para los Amigos de Casa </h2>
+            <div class="comida-descp-perro">
+                <ul>
+                    <li>Croquetas</li>
+                    <a href="https://www.expertoanimal.com/alimentacion-del-perro-segun-su-edad-21196.html" id="bot1"> Ver más </a>
+                    <li>Frutas y Verduras</li>
+                    <a href="https://www.petdarling.com/articulos/frutas-para-perros/" id="bot2"> Ver mas </a>
+                    <li>Carnes </li>
+                    <a href="https://revista.weepec.com/que-tipos-de-carne-puede-comer-un-perro/" id="bot3"> Ver más </a>
+                </ul>
+            </div>
+
+            <div class="comida-descp-gato">
+                <ul>
+                    <li>Croquetas</li>
+                    <a href="https://www.expertoanimal.com/alimentacion-del-perro-segun-su-edad-21196.html" id="bot1"> Ver más </a>
+                    <li>Frutas y Verduras</li>
+                    <a href="https://www.petdarling.com/articulos/frutas-para-perros/" id="bot2"> Ver mas </a>
+                    <li>Carnes </li>
+                    <a href="https://revista.weepec.com/que-tipos-de-carne-puede-comer-un-perro/" id="bot3"> Ver más </a>
+                </ul>
+            </div>
+
+            <div class="imgs">
+                <img src="https://cdn.pixabay.com/photo/2016/06/14/00/14/cat-1455468_1280.jpg" alt="" id="img1">
+                <img src="https://cdn.pixabay.com/photo/2016/03/27/21/16/animal-1284307_1280.jpg" alt="" id="img2">
+            </div>
+            
+        </div>
     </div>
     <div class="razas">
         <span>Algunas Razas de Perros Interesantes</span>
@@ -31,7 +71,7 @@
     <div class="preguntas">
         <h3>Preguntas Frecuentes sobre Ellos</h3>
         <ul>
-            <li></li>
+            <li>Que </li>
             <li></li>
             <li></li>
             <li></li>
