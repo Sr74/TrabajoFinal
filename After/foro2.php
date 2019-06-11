81314d16e31a8f0fdc64b75bd36f557d98846191
Jairo Castro <Jairo182278@gmail.com>
2019-06-10 19:23:56 -0500

    foro2 es la segunda opcion de estructura

--- After/foro2.php
+++ After/foro2.php
@@ -10,6 +10,7 @@ $sql2 = "SELECT*FROM respuestas ";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="foro2.css">
 </head>
 <body>
     <form action="procesoforo.php" method="post">
@@ -20,39 +21,36 @@ $sql2 = "SELECT*FROM respuestas ";
         </div>
     </form>
     <br>
-    <table class="table">
+    
         
         <?php
         foreach($pdo->query($sql) as $fila){
             
         ?> 
-            <tr>
-                <td><textarea cols="30" rows="5" readonly><?php echo $fila["pregunta"]?></textarea></td>
-                <td>
-                    <form action="procesorespuesta.php?id=<?php echo $fila["id"]?>" method="post">
-                        <textarea name="respuesta" cols="30" rows="2" placeholder="Pon una respuesta"></textarea>
-                        <button>enviar</button>
-                    </form>
-                    <?php  $dato = $fila["id"] ?>
-                </td>
+            <div class="orden">
+                <textarea cols="30" rows="5" readonly><?php echo $fila["pregunta"]?></textarea>
                 
-            </tr>
-            <tr>
-                <td>
-                    <?php
-                    foreach($pdo->query($sql2) as $fila2){
-                        if($fila2["idpre"]==$fila["id"]){
-                    ?>
+                <form action="procesorespuesta.php?id=<?php echo $fila["id"]?>" method="post">
+                    <div class="orden">
+                        <textarea name="respuesta" cols="30" rows="2" placeholder="Pon una respuesta"></textarea>
+                        <input type="submit" values="enviar">
+                    </div>
+                </form>
+                    <?php  $dato = $fila["id"] ?>
+            </div>  
+
+            <?php
+            foreach($pdo->query($sql2) as $fila2){
+                if($fila2["idpre"]==$fila["id"]){
+            ?>
+
                         <textarea cols="50" rows="5" readonly ><?php echo $fila2["respuesta"]?></textarea><br>
-                    <?php
-                        }
-                    }
-                    ?>
-                </td>
-            </tr>
         <?php
+               }
+            }
+        
         }
         ?>
-    </table>
+   
 </body>
 </html>
