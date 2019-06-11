25fc8bf3b27af4327379beb698c5da57099e33a5
david huanachin <lo.kito123@live.com>
2019-06-11 16:41:19 -0500

    diseño

--- After/daralerta.php
+++ After/daralerta.php
@@ -5,31 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="page2.php">
+    <link rel="stylesheet" href="alerta.css">
 </head>
 <body>
-    <?php include("menu.php" ?>
+    <?php include("menu.php" )?>
+    <div class="alerta">
     <form action="procesoalerta.php" method="post">
+        <h1>Publicar alerta</h1><br>
         <div>
             <label>Ingrese nombre</label>
             <input type="text" name="nombre" placeholder="No es obligatorio">
-        </div>
+        </div><br>
         <div>
             <label>Ingrese un telefono</label>
             <input type="text" name="telefono" placeholder="No es obligatorio">
-        </div>
+        </div><br>
         <div>
             <label>Ingrese la direccion</label>
             <input type="text" name="direc" placeholder="Obligatorio">
-        </div>
+        </div><br>
         <div>
-            <label>Ingrese una descripcion</label>
+            <label>Ingrese una descripcion</label><br>
             <textarea name="descrip" cols="30" rows="10" placeholder="Obligatorio"></textarea>
-        </div>
+        </div><br>
         <div>
-            <button>Enviar</button>
+            <button class="bott">Enviar</button>
         </div>
     </form>
+
+    </div>
+    
     
 </body>
 </html>
